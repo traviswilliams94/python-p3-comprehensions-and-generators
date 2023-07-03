aa549429119e77ff612403fabc1c0358ba775fca
Travis <travis.williams94@gmail.com>
2023-07-03 10:58:08 -0600

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [i for i in num_list if i % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [(i + "!") for i in sentence_list]
